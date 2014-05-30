aef0e0b41a4cffec44abc96e558cdf0a92e062a2
Brandonthom <Brandonthom@gmail.com>
2014-05-30 03:54:10 -0400

    Correct VS Project Type

---
+++ WebApplicationDevelopment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationDevelopment.Startup))]
+namespace WebApplicationDevelopment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
