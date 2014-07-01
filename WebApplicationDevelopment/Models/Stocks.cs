51bc1ed44647a4db0283671ea08d8248a3259693
Brandonthom <Brandonthom@gmail.com>
2014-07-01 14:43:09 -0400

    Stock Database Added
    A Database to hold stock information has been added and a connection
    string is setup.

---
+++ WebApplicationDevelopment/Models/Stocks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebApplicationDevelopment.Models
+{
+    public class Stocks
+    {
+        public String symbol { get; set; }
+        public float currentPrice { get; set;}
+    }
+
+    public class StocksDBContext : DbContext
+    {
+        public  DbSet <Stocks> Stocks { get; set; }
+    }
+}
