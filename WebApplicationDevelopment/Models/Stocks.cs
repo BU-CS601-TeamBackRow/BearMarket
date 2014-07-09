2df9d6691e9685a33b7012db5a5837a8ee486ca2
Brandonthom <Brandonthom@gmail.com>
2014-07-09 18:47:49 -0400

    Added Stocks Controller

--- WebApplicationDevelopment/Models/Stocks.cs
+++ WebApplicationDevelopment/Models/Stocks.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace WebApplicationDevelopment.Models
 {
     public class Stocks
     {
+        [Key]
         public String symbol { get; set; }
         public float currentPrice { get; set;}
     }
