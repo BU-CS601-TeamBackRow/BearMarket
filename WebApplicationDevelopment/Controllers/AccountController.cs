ad2541dff5e9ee85e15ebffb3bd0b91db26128cb
JT Green <jonathan_green11@yahoo.com>
2014-07-25 09:23:55 -0400

    Account controller updates

--- WebApplicationDevelopment/Controllers/AccountController.cs
+++ WebApplicationDevelopment/Controllers/AccountController.cs
@@ -102,7 +102,7 @@ namespace WebApplicationDevelopment.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("MyAccounts", "Account");
                 }
                 else
                 {
